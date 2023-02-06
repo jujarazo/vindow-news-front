7baa848fb4cfdb9feba74842326d70bcea44c7aa
JuJarazo <julianjarazo94@gmail.com>
2023-02-05 22:44:09 -0300

    Add search validation

--- src/components/Header/Search/Search.tsx
+++ src/components/Header/Search/Search.tsx
@@ -1,10 +1,24 @@
-import { Field, Form, Formik } from 'formik';
+import { Field, Form, Formik, ErrorMessage } from 'formik';
 import { useContext } from 'react';
 import NewsContext from '../../../context/NewsContext';
 import { Spinner } from '../../Commons';
 
+type searchValues = {
+  searchTerm: string;
+};
+
 export function Search() {
   const { isLoadingNews, handleSearchNews } = useContext(NewsContext);
+
+  const searchValidation = (values: searchValues) => {
+    const errors: Partial<searchValues> = {};
+
+    if (!values.searchTerm || values.searchTerm.length < 3) {
+      errors.searchTerm = 'The term you must be at least 3 characters long';
+    }
+
+    return errors;
+  };
 
   return (
     <Formik
@@ -14,21 +28,29 @@ export function Search() {
       onSubmit={async (values) => {
         await handleSearchNews(values.searchTerm, 1);
       }}
+      validate={searchValidation}
     >
-      <Form className="d-flex">
-        <Field
-          className="form-control"
+      <Form>
+        <div className="d-flex">
+          <Field
+            className="form-control"
+            name="searchTerm"
+            placeholder="News search..."
+            type="text"
+          />
+          <button
+            type="submit"
+            className="btn btn-primary ms-3"
+            disabled={isLoadingNews}
+          >
+            {isLoadingNews ? <Spinner /> : 'Search'}
+          </button>
+        </div>
+        <ErrorMessage
+          className="text-danger"
           name="searchTerm"
-          placeholder="News search..."
-          type="text"
+          component="div"
         />
-        <button
-          type="submit"
-          className="btn btn-primary ms-3"
-          disabled={isLoadingNews}
-        >
-          {isLoadingNews ? <Spinner /> : 'Search'}
-        </button>
       </Form>
     </Formik>
   );
